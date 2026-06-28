d0ed3ff57f31c0687fe7c3fe1f49aaeed8cbe708
Matt Megaard <mmegaard@users.noreply.github.com>
2026-06-28 10:19:40 -0700

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        combos = []
+        combo = []
+        
+        def dfs(i):
+            if i == len(nums):
+                combos.append(combo.copy())
+                return
+            combo.append(nums[i])
+            dfs(i + 1)
+            combo.pop()
+            dfs(i + 1)
+        dfs(0)
+        return combos
+                
