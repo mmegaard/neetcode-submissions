5a06218f89d1aed4774ddb90f7c5c973968660b9
Matt Megaard <mmegaard@users.noreply.github.com>
2026-09-10 10:47:17 -0700

    Add: course-schedule - submission-8

---
+++ Data Structures & Algorithms/course-schedule/submission-8.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereqs = {i:set() for i in range(numCourses)}
+        taken = set()
+        for cur,pre in prerequisites:
+            prereqs[cur].add(pre)
+        
+        def dfs(course):
+            if course in taken:
+                print(course, 'in taken', course, taken)
+                return False
+            numofprereqs = len(prereqs[course])
+            if numofprereqs == 0:
+                return True
+            taken.add(course)
+            for p in prereqs[course]:
+                if not dfs(p):
+                    return False  
+            taken.remove(course)
+            prereqs[course] = []
+            return True
+                
+
+        for c in prereqs:
+            if not dfs(c):
+                print('its false')
+                return False
+        return True
+        
+        
