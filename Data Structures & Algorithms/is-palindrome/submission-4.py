3d0d702807d9fe8bcad15611fac13439b1df2e64
Matt Megaard <mmegaard@users.noreply.github.com>
2026-09-15 13:38:47 -0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        first = 0
+        second = len(s) - 1
+        while first <= second:
+            firstChar = s[first].lower()
+            secondChar = s[second].lower()
+            if not firstChar.isalnum():
+                first += 1
+                continue
+            if not secondChar.isalnum():
+                second -= 1
+                continue
+            if first != second and firstChar != secondChar:
+                return False
+            first += 1
+            second -= 1
+        return True
